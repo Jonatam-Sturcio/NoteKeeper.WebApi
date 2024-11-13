0db674d2139090c2cc2e3dec1855992ce47d6653
Jonatam-Sturcio <bitcomanderbr@gmail.com>
2024-11-13 14:09:36 -0300

    Refatoração de injeção de dependencia da WebAPi

--- server/NoteKeeper.WebApi/Program.cs
+++ server/NoteKeeper.WebApi/Program.cs
@@ -1,16 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using NoteKeeper.Aplicacao.ModuloCategoria;
-using NoteKeeper.Aplicacao.ModuloNota;
-using NoteKeeper.Dominio.Compartilhado;
-using NoteKeeper.Dominio.ModuloCategoria;
-using NoteKeeper.Dominio.ModuloNota;
-using NoteKeeper.Infra.Orm.Compartilhado;
-using NoteKeeper.Infra.Orm.ModuloCategoria;
-using NoteKeeper.Infra.Orm.ModuloNota;
-using NoteKeeper.WebApi.Config;
-using NoteKeeper.WebApi.Config.Mapping;
-using NoteKeeper.WebApi.Config.Mapping.Action;
-using NoteKeeper.WebApi.Filters;
+﻿using NoteKeeper.WebApi.Config;
 using Serilog;
 
 namespace NoteKeeper.WebApi;
@@ -21,57 +9,24 @@ public class Program
 	{
 		const string politicaCors = "_minhaPoliticaCors";
 
-		//Configuração de Injeção
 		var builder = WebApplication.CreateBuilder(args);
 
-		var connectionString = builder.Configuration["SQL_SERVER_CONNECTION_STRING"];
+		builder.Services.ConfigureDbContext(builder.Configuration);
 
-		builder.Services.AddDbContext<IContextoPersistencia, NoteKeeperDbContext>(optionsBuilder =>
-			{
-				optionsBuilder.UseSqlServer(connectionString, dbOptions =>
-				{
-					dbOptions.EnableRetryOnFailure();
-				});
-			});
+		builder.Services.ConfigureCoreServices();
 
-		builder.Services.AddScoped<IRepositorioCategoria, RepositorioCategoriaOrm>();
-		builder.Services.AddScoped<ServicoCategoria>();
+		builder.Services.ConfigureAutoMapper();
 
-		builder.Services.AddScoped<IRepositorioNota, RepositorioNotaOrm>();
-		builder.Services.AddScoped<ServicoNota>();
-		builder.Services.AddScoped<ConfigurarCategoriaMappingAction>();
+		builder.Services.ConfigureCors(politicaCors);
 
-		builder.Services.AddAutoMapper(config =>
-		{
-			config.AddProfile<CategoriaProfile>();
-			config.AddProfile<NotaProfile>();
-		});
-
-		//Configuração politica Cors
-		builder.Services.AddCors(options =>
-		{
-			options.AddPolicy(name: politicaCors, policy =>
-			{
-				policy
-				.AllowAnyOrigin()
-				.AllowAnyHeader()
-				.AllowAnyMethod();
-			});
-		});
-
-		builder.Services.AddControllers(options =>
-		{
-			options.Filters.Add<ResponseWrapperFilter>();
-		});
+		builder.Services.ConfigureControllersWithFilters();
 
 		builder.Services.AddEndpointsApiExplorer();
 
 		builder.Services.AddSwaggerGen();
 
-		var key = builder.Configuration["SERILOG_LICENSE_KEY"];
-		builder.Services.ConfigureSerilog(builder.Logging, key);
+		builder.Services.ConfigureSerilog(builder.Logging, builder.Configuration);
 
-		//Execução da API
 		var app = builder.Build();
 
 		app.UseGlobalExceptionHandler();
